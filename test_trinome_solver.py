b4188da07d82500b585de9abf5b24152d23adfd2
Adrien Raison <adrien.raison@univ-poitiers.fr>
2022-09-12 15:13:35 +0200

    Adding unittest

--- test_trinome_solver.py
+++ test_trinome_solver.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#  import argparse
+import argparse
 from td1.trinome_solver import solver
-import unittest
 
-#  parser = argparse.ArgumentParser()
+#  import unittest
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-a", help="Value of a", required=True)
+parser.add_argument("-b", help="Value of b", required=True)
+parser.add_argument("-c", help="Value of c", required=True)
+
+args = parser.parse_args()
+
+config = [args.a, args.b, args.c]
+res = solver(*config)
+print(res)
+
+#  class TestUtils(unittest.TestCase):
+#  def test_solver(self):
+#  self.assertEqual(solver(*config1), [float(1), float(2)])
+#  self.assertEqual(solver(*config2), [float(1), float(1)])
+#  self.assertEqual(solver(*config3), [])
+#  self.assertRaises(ValueError, solver, *config4)
 #
-#  parser.add_argument("-a", help="Value of a", required=True)
-#  parser.add_argument("-b", help="Value of a", required=True)
-#  parser.add_argument("-c", help="Value of a", required=True)
 #
-#  args = parser.parse_args()
-#
-config1 = [1, -3, 2]
-config2 = [1, -2, 1]
-config3 = [1, 0, 3]
-config4 = [0, 0, 3]
-
-
-class TestUtils(unittest.TestCase):
-    def test_solver(self):
-        self.assertEqual(solver(*config1), [float(1), float(2)])
-        self.assertEqual(solver(*config2), [float(1), float(1)])
-        self.assertEqual(solver(*config3), [])
-        self.assertRaises(ValueError, solver, *config4)
-
-
-if __name__ == "__main__":
-    unittest.main()
+#  if __name__ == "__main__":
+#  unittest.main()
