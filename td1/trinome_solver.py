81c6333a8cf060e8c11ba4e76477148d53ee7ff6
Adrien Raison <adrien.raison@univ-poitiers.fr>
2022-09-12 14:16:03 +0200

    Fisrt commit

---
+++ td1/trinome_solver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+from typing import List
+
+# Let a,b,c in R with a=/=0, what is the root of P(x)=ax^2+bx+c=0 ? Propose an algorithm that provide roots in case in P(x) is splitable on R !
+
+
+def solver(a: float, b: float, c: float) -> List:
+    if a == 0:
+        raise ValueError
+    delta = b ** 2 - 4 * a * c
+    if delta > 0:
+        return [(-b + val * np.sqrt(delta)) / (2 * a) for val in [-1, 1]]
+    elif delta == 0:
+        return [-b / (2 * a) for _ in range(2)]
+    else:
+        return []
